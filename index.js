14a1c0661c4428514b7a3e868222984571ed9e0c
Luis Miguel del Corral Cuervo <luisdelcorral@corporativo.adams.es>
2019-02-27 10:38:54 +0100

    Update index.js
    Insertamos heroku-postgresql

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
